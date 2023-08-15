7da72dba9630f5d2af1d5b26ba0fce73ed4e7158
IsaltinaPepete <isaltinabrizito@gmail.com>
2023-08-15 13:37:45 -0700

    task 0

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ */
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter < 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+}
