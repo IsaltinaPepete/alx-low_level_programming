80db27947c4d09c9b3bf89cd8b0031966605259f
IsaltinaPepete <isaltinabrizito@gmail.com>
2023-08-15 14:00:26 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet 10x
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char letter = 'a';
+
+	for (i = 0; i < 10; i++)
+	{
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+		letter = 'a';
+		putchar('\n');
+	}
+	_putchar('\n');
+}
