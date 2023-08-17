31451771b77e6e5b474c39fc3a8ed004c618c579
IsaltinaPepete <isaltinabrizito@gmail.com>
2023-08-17 07:38:43 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * isupper - is upper
+ * @c: paramiter
+ * Return: 1 or 0
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	return (0);
+}
