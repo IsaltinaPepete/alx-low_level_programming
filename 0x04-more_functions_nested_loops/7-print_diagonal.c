36fbc16b3d335c850ea53f9867e5bc2be15b306d
IsaltinaPepete <isaltinabrizito@gmail.com>
2023-08-17 14:08:06 -0700

    task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print diagonal
+ * @n: paramiter
+ */
+void print_diagonal(int n)
+{
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+		}
+	}
+	_putchar('\n');
+}
