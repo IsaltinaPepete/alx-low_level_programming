9388f375d889c89a5fbce3aaa94e177f6e1667c8
Isaltina Brizito José Francisco Pepete <116559992+IsaltinaPepete@users.noreply.github.com>
2023-08-22 15:58:26 +0200

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,7 +7,6 @@
 void rev_string(char *s)
 {
 	int i, len, j;
-	char *new_string;
 
 	j = 0;
 	len = 0;
@@ -16,6 +15,8 @@ void rev_string(char *s)
 	{
 		len++;
 	}
+
+	char new_string[len];
 
 	for (i = len - 1; i >= 0; i--)
 	{
