a214c871aceb81dbb3f5b12fd707cb73382b319c
IsaltinaPepete <isaltinabrizito@gmail.com>
2023-09-06 03:00:27 -0700

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *_strcat - something
+ * @dest: paramiter
+ * @src: paramiter
+ * Return: pointer
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *pointer = dest;
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	return *pointer;
+}
