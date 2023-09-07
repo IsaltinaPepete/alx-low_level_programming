180b287823495a32dc6c4005c8dc8a8663177b8a
IsaltinaPepete <isaltinabrizito@gmail.com>
2023-09-07 07:22:19 -0700

    task 02

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,16 +13,18 @@ char *_strncpy(char *dest, char *src, int n)
 
 	i = 0;
 
-	while (src[i] != '\0')
+	while (src[i] != '\0' && n > 0)
 	{
 		dest[i] = src[i];
 		i++;
+		n--;
 	}
 
-	while (dest[i] != '\0')
+	while (n > 0)
 	{
 		dest[i] = '\0';
 		i++;
+		n--;
 	}
 
 	return (dest);
