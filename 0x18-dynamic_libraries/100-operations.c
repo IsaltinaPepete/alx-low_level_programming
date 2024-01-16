f22bdf6616ce8eca8dc4dce34bde7f52cce5a78a
IsaltinaPepete <isaltinabrizito@gmail.com>
2024-01-16 01:43:15 -0800

    task 03

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,60 @@
+int add(int i, int k);
+int sub(int i, int k);
+int mul(int i, int k);
+int div(int i, int k);
+int mod(int i, int k);
+
+/**
+* add - adds two integers
+* @i: first integer to add
+* @k: second integer to add
+* Return: the sum
+*/
+int add(int i, int k)
+{
+	return (i + k);
+}
+
+/**
+* add - sub two integers
+* @i: first integer to sub
+* @k: second integer to sub
+* Return: the sub
+*/
+int sub(int i, int k)
+{
+	return (i - k);
+}
+
+/**
+* add - mul two integers
+* @i: first integer to mul
+* @k: second integer to mull
+* Return: the mul
+*/
+int mul(int i, int k)
+{
+	return (i * k);
+}
+
+/**
+* add - adds two integers
+* @i: first integer to add
+* @k: second integer to add
+* Return: the div
+*/
+int div(int i, int k)
+{
+	return (i / k);
+}
+
+/**
+* add - adds two integers
+* @i: first integer to add
+* @k: second integer to add
+* Return: the sum
+*/
+int mod(int i, int k)
+{
+	return (i % k);
+}
