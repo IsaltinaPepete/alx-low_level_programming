bbdde2e8e08370595fc1c0ba94a7ea5c866945d6
IsaltinaPepete <isaltinabrizito@gmail.com>
2024-01-17 05:12:44 -0800

    task 0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/*
+ * print_dlistint - print elements of a list
+ * @h: var
+ * Return: cont
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int cont = 0;
+
+	if (h == NULL)
+	{
+		return (cont);
+	}
+
+	if (h->prev != NULL)
+	{
+		h = h->prev;
+	}
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		cont++;
+		h = h->next;
+	}
+
+	return (cont);
+}
